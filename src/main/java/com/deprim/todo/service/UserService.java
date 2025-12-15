cf0316d2ac56fe4e0f3c12a74f145ab54ee4948c
deprim <webomey123@gmail.com>
2025-12-15 17:23:32 +0200

    Add `findByUsername` method in UserService for fetching user by username

--- src/main/java/com/deprim/todo/service/UserService.java
+++ src/main/java/com/deprim/todo/service/UserService.java
@@ -1,8 +1,11 @@
 package com.deprim.todo.service;
 
+import com.deprim.todo.model.User;
 import com.deprim.todo.reposotpry.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -12,5 +15,9 @@ public class UserService {
     @Autowired
     public UserService(UserRepository userRepository) {
         this.userRepository = userRepository;
+    }
+
+    public Optional<User> findByUsername(String username){
+        return userRepository.findByUsername(username);
     }
 }
